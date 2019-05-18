9e88f8be2b59b53956a13502b2ce5700c9425d48
CaameronNakasone <caameron@pdx.edu>
2019-05-17 21:21:47 -0700

    added in buttons to navigate back and fourth between home and results page :

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ var server = express();
 //Home Page
 server.get('/', (req, res) => {
   res.status(200);
-  res.send('<!DOCTYPE html><html><body>HOME</body></html>');
+  res.sendFile(path.join(__dirname + '/home.html'))
+  // res.send('<!DOCTYPE html><html><body>HOME</body></html>');
 });
 
 //Results Page
 server.get('/results', (req, res) => {
   res.status(200);
-  res.send('<!DOCTYPE html><html><body>Results page</body></html>');
+  res.sendFile(path.join(__dirname + '/results.html'))
 });
 
 server.listen(process.env.PORT || 3000);
