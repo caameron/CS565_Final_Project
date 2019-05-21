7ba63452733ddd17f5b258036e0badb96e632fb6
CaameronNakasone <caameron@pdx.edu>
2019-05-21 11:04:36 -0700

    made a simple call to places api in server file

--- server.js
+++ server.js
@@ -4,12 +4,33 @@ var express = require('express');
 var path = require('path');
 
 var server = express();
+var googleMapsClient = require('@google/maps').createClient({
+  key: 'AIzaSyDv4A1my3d9B16HqH_6tHPXwSbAoZILZx4'
+});
+
 
 //Home Page
 server.get('/', (req, res) => {
   res.status(200);
   res.sendFile(path.join(__dirname + '/home.html'))
   // res.send('<!DOCTYPE html><html><body>HOME</body></html>');
+  // googleMapsClient.places({
+  //   query: 'fast food',
+  //   language: 'en',
+  //   location: [-33.865, 151.038],
+  //   radius: 5000,
+  //   minprice: 1,
+  //   maxprice: 4,
+  //   opennow: true,
+  //   type: 'restaurant'
+  // }, (err, response) => {
+  //   if(!err){
+  //     console.log(response.json.results);
+  //   }
+  //   else{
+  //     console.log(err);
+  //   }
+  // });
 });
 
 //Results Page
